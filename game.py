a1dd2180bdb7b146903e48ac45e898a8090bb08c
Maruti745 <129722665+Maruti745@users.noreply.github.com>
2023-07-16 21:24:59 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,17 +1,1 @@
-import random
-options=['rock','paper','scissors']
-computer_choice=random.choice(options)
-user_choice=input('enter your choice=').lower()
-if user_choice=='rock and computer_choice=='scissors':
-print('you won')
-elif user_choice=='paper' and computer_choice=='rock':
-print('you won')
-elif user_choice=='scissors' and computer_choice=='paper':
-print('you won')
-elif 
-user_choice==computer_choice:
-print('you tied')
-else:
-  print(f'you lost.The computer picked {computer_choice}')
-  
-  
+
